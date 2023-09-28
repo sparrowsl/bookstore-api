256d4e6a40f8acae1a9c5c7e19fed55312f28adc
sparrowsl <benthorpe.dev@gmail.com>
2023-09-28 08:12:24 +0000

    add more routes for books

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+books = ["Harry Potter", "Chronicles of Narnia",
+         "Atomic Habits", "Learning Python", "Learning Go"]
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home():
+    return {"message": "Welcome to bookstore!"}
+
+
+@app.get("/books")
+async def get_books():
+    return {"books": books}
+
+
+@app.get("/books/{index}")
+async def get_book(index: int):
+    if index not in range(1, len(books) + 1):
+        return {"message": "No book found"}
+
+    return {"book": books[index - 1]}
